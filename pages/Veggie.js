94af210f63ed9e69c39e1aabceffefe46919405b
Jerika Kanz <jerikacapila@gmail.com>
2022-10-11 16:56:47 +0200

    Image From Public Directory

--- pages/Veggie.js
+++ pages/Veggie.js
@@ -1,3 +1,23 @@
+import Image from "next/image";
+import pizza from "../public/images/pizza.jpg";
 export default function Veggie() {
-  return <div>Veggie</div>;
+  return (
+    <div>
+      <h2>Veggie</h2>
+      <p>
+        Some Dummy Text Some Dummy Text Some Dummy TextSome Dummy Tex Some Dummy
+        TextSome Dummy TextSome Dummy Text Some Dummy Text Some Dummy Text Some
+        Dummy Text Some Dummy Text Some Dummy Text Some Dummy Text Some Dummy
+        Text Some Dummy Text Some Dummy Text Some Dummy Text Some Dummy Text
+        Some Dummy TextSome Dummy Text
+      </p>
+      <Image
+        src={pizza}
+        alt="Picture of the pizza"
+        width={500}
+        height={300}
+        layout="responsive"
+      />
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Veggie from "./Veggie";
 
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1 className={styles.main}>Hello World</h1>
+      <Veggie />
+    </>
+  );
 }
