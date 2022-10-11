62a3518292c023b6749ccfcd4869a9285f87a938
Jerika Kanz <jerikacapila@gmail.com>
2022-10-11 17:17:04 +0200

    External Domain

--- pages/Dessert.js
+++ pages/Dessert.js
@@ -1,3 +1,26 @@
+import Image from "next/image";
+
 export default function Dessert() {
-  return <div>Dessert</div>;
+  const photoUrl =
+    "https://unsplash.com/photos/surQ2mkZNxw/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mzd8fGRlc3NlcnR8ZW58MHwwfHx8MTY1OTMzNzk1NQ&force=true&w=2400";
+
+  return (
+    <>
+      <h2>Dessert</h2>
+      <p>
+        Some Dummy Text Some Dummy Text Some Dummy TextSome Dummy Tex Some Dummy
+        TextSome Dummy TextSome Dummy Text Some Dummy Text Some Dummy Text Some
+        Dummy Text Some Dummy Text Some Dummy Text Some Dummy Text Some Dummy
+        Text Some Dummy Text Some Dummy Text Some Dummy Text Some Dummy Text
+        Some Dummy TextSome Dummy Text
+      </p>
+      <Image
+        src={photoUrl}
+        alt="Picture of the pizza"
+        // width={500}
+        // height={300}
+        layout="responsive"
+      />
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Veggie from "./Veggie";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <h1 className={styles.main}>Hello World</h1>
-      <Veggie />
+      <p>This is my new NextJS project</p>
+      <Link href="/Veggie">Veggie</Link>
+      <Link href="/Dessert">Dessert</Link>
     </>
   );
 }
